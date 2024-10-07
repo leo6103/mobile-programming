5bb48fe4f4fa266cfe94e4d7312cdceab76d02ef
Phan Khoi Nguyen <nguyenpk@bsscommerce.com>
2024-10-07 15:55:33 +0700

    Init repo

---
+++ src/main/java/com/app/debug/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.app.debug
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val usernameEditText = findViewById<EditText>(R.id.username)
+        val passwordEditText = findViewById<EditText>(R.id.password)
+        val loginButton = findViewById<Button>(R.id.login_button)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            Log.d("LoginDebug", "Username: $username, Password: $password")
+        }
+    }
+}
